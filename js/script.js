e4b3bee5d6764f15d7a418450962360e14403ce8
Javier Florian <javflori@prodigious.com>
2021-11-21 22:04:17 -0500

    adding mailto

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-var boton = document.getElementById('enviar')
+var formulario = document.getElementById("email-formulario");
 
-function eventoClick() {
-  console.log('Click just happened')
-}
-
-if(boton){
-  boton.addEventListener('submit', eventoClick, false);
-}
+formulario.addEventListener("submit", function (e) {
+  var desea_enviar_correo = confirm("Deseas enviar el correo");
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var mensaje = document.getElementById("mensaje").value;
+  if (!desea_enviar_correo) {
+    e.preventDefault();
+  }
+  else {
+    alert("Enviando correo a -> " + name + " con el siguiente mensaje \n ______ \n  " + mensaje);
+    alert("Mensaje enviado a -> " + email);
+    e.preventDefault();
+    window.open("mailto:contacto@heavensoftware.com.co?subject="+ name + "&body=" + mensaje);
+  }
+});
